a53158a628b6b55eda45d3a82e6f05c823ccf4a4
gamh86 <g.amh.86@outlook.fr>
2020-06-28 00:01:14 +0100

    Add additional constructor

--- src/main/java/GenericTextField.java
+++ src/main/java/GenericTextField.java
@@ -5,13 +5,22 @@ import java.awt.Font;
 
 public class GenericTextField extends JTextField
 {
-	private Font font = new Font("Courier New", Font.PLAIN, 19);
+	private Font font = new Font("Courier New", Font.PLAIN, 16);
 
 	public GenericTextField(String s)
 	{
 		super(s);
 
 		setFont(font);
+		setComponentPopupMenu(new RightClickPopup().getMenu());
+	}
+
+	public GenericTextField(String s, int fontStyle)
+	{
+		super(s);
+
+		Font _font = new Font("Courier New", fontStyle, 16);
+		setFont(_font);
 		setComponentPopupMenu(new RightClickPopup().getMenu());
 	}
 }
