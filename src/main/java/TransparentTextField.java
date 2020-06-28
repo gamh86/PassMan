82b4a2ac3f963bd9a030aaae76889e756698af3b
gamh86 <g.amh.86@outlook.fr>
2020-06-28 16:49:17 +0100

    Override paintComponent() method

--- src/main/java/TransparentTextField.java
+++ src/main/java/TransparentTextField.java
@@ -9,16 +9,24 @@ public class TransparentTextField extends JTextField
 {
 	private final int TF_WIDTH = 400;
 	private final int TF_HEIGHT = 27;
+	private final Color color = new Color(255, 255, 255, 0);
 
 	public TransparentTextField(String s)
 	{
 		super(s);
 
-		setBackground(new Color(240, 240, 240));
 		setBorder(null);
 		setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		setPreferredSize(new Dimension(TF_WIDTH, TF_HEIGHT));
 		setEditable(false);
 		setComponentPopupMenu(new RightClickPopup().getMenu());
+	}
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		g.setColor(color);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		super.paintComponent(g);
 	}
 }
