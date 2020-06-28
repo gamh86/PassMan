a16ea12c64270c38937caf5311a2d4d05e2bc4f2
gamh86 <g.amh.86@outlook.fr>
2020-06-28 16:47:46 +0100

    Override paintComponent() method

--- src/main/java/TransparentButton.java
+++ src/main/java/TransparentButton.java
@@ -1,16 +1,31 @@
 package custom;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.ImageIcon;
+import java.awt.Graphics;
 import java.awt.Color;
 
 public class TransparentButton extends JButton
 {
+	private Color color = null;
+
 	public TransparentButton(ImageIcon icon)
 	{
 		super(icon);
+		super.setContentAreaFilled(false);
 
-		setBackground(new Color(240, 240, 240));
+		color = new Color(255, 255, 255, 0);
+
+		setBackground(color);
 		setBorder(null);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		g.setColor(color);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		super.paintComponent(g);
 	}
 }
